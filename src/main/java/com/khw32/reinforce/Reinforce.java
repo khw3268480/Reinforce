519fc1c76bf244f8ca1c875739e813abf7ebb023
khw3268480 <khw3268480@naver.com>
2024-02-19 04:13:20 +0900

    first commit

---
+++ src/main/java/com/khw32/reinforce/Reinforce.java
@@ -0,0 +1,17 @@
+package com.khw32.reinforce;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Reinforce extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
